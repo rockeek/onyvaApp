500078dd75b28373ab1760a21b1132da693314d1
Remy Chatti <rchatti@jw.org>
2017-09-29 16:45:05 +0200

    Undo Try real life connection

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,9 +1,8 @@
 export class Config {
     // PROD:
     // static serverUrl = "http://boogy.ovh/onyva/";
-    // https://hphp-rockeek.c9users.io
+
     //C9:
-    //static serverUrl = "/";
-    static serverUrl = "https://hphp-rockeek.c9users.io/";
+    static serverUrl = "/";
     static clientVersion = 1;
 }
