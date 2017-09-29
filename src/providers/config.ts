7bdeff38fefc4e7b11347bb4c273b4fad69efc3c
Remy Chatti <rchatti@jw.org>
2017-09-29 16:43:52 +0200

    Try real life connection

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,8 +1,9 @@
 export class Config {
     // PROD:
     // static serverUrl = "http://boogy.ovh/onyva/";
-
+    // https://hphp-rockeek.c9users.io
     //C9:
-    static serverUrl = "/";
+    //static serverUrl = "/";
+    static serverUrl = "https://hphp-rockeek.c9users.io/";
     static clientVersion = 1;
 }
