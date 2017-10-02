5c03e6b8ffbcdc22eb2f7d2f3b87e5045182f870
Remy Chatti <rchatti@jw.org>
2017-10-02 11:25:37 +0200

    Test prod

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,8 +1,10 @@
 export class Config {
     // PROD:
     // static serverUrl = "http://boogy.ovh/onyva/";
+    // static serverUrl = "http://boogy.ovh/";
 
     //C9:
-    static serverUrl = "/";
+    // static serverUrl = "/";
+    static serverUrl = "http://boogy.ovh/";
     static clientVersion = 1;
 }
