5b692ebd53e89cdf40c7f03928c680dff95e5a55
Remy Chatti <rchatti@jw.org>
2017-10-10 18:00:18 +0200

    Better isDev mode

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
+import {Config} from '../../providers/config'
 
 @Component({
     selector: 'page-welcome',
     templateUrl: 'welcome.html'
 })
 export class WelcomePage {
-    public isDev: boolean = ((<any>window)['IonicDevServer'] != undefined);
+    constructor(private config: Config) {}
 }
