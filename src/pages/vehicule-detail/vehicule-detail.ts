5d3ade7762461bb5ce822985eac6cac12ef61e33
Remy Chatti <rchatti@jw.org>
2017-10-11 14:15:46 +0200

    Vehicule detail as form

--- src/pages/vehicule-detail/vehicule-detail.ts
+++ src/pages/vehicule-detail/vehicule-detail.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { VehiculeService } from '../../providers/vehicule.service';
 import { Vehicule } from '../../app/vehicule';
+import {
+	FormBuilder,
+	FormGroup,
+  	Validators
+} from '@angular/forms';
 
 @Component({
     selector: 'page-vehicule-detail',
@@ -11,25 +16,39 @@ import { Vehicule } from '../../app/vehicule';
     ]
 })
 export class VehiculeDetailPage {
+    public form: FormGroup;
     callback: any;
     vehicule: Vehicule;
+    displayedName: string;
     errorMessage: string;
     isLoading: boolean;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, public service: VehiculeService) {
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public service: VehiculeService,
+        private _FB: FormBuilder) {
         // Copy the passed object to break the bindings.
         // If we don't, when we go back, the vehicule appears modified.
         this.vehicule = Object.assign({}, this.navParams.get('vehicule'));
         this.callback = this.navParams.get('callback');
+        this.displayedName = this.vehicule.vehiculeId == null ? '[new]' : this.vehicule.name;
+        this.form = _FB.group({
+            'vehiculeId': [this.vehicule.vehiculeId],
+            'name': [this.vehicule.name, Validators.required],
+            'trademark' : [this.vehicule.trademark, Validators.required],
+            'color' : [this.vehicule.color, Validators.required],
+            'seats' : [this.vehicule.seats, Validators.required]
+        });
     }
 
-    save(): void {
+    save(vehicule: Vehicule): void {
         this.isLoading = true;
 
         // Call the vehiculeListPage's callback so that, once saved, 
         // the server's response refreshes the list of vehicules automatically
         // Trick from // https://forum.ionicframework.com/t/solved-ionic2-navcontroller-pop-with-params/58104/4
-        this.callback(this.vehicule).then(()=>{
+        this.callback(vehicule).then(()=>{
             this.isLoading = false;
             this.navCtrl.pop();
          });
